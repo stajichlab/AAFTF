78c791808be9fde4a77e44caa51fe58d102234b5
Jason Stajich <jasonstajich.phd@gmail.com>
2018-07-31 00:53:33 -0700

    coding up some URLs for web-base data resources

---
+++ AAFTF/resources.py
@@ -0,0 +1,11 @@
+
+Contaminant_Accessions = {"phiX": 'ftp://ftp.ncbi.nlm.nih.gov/genomes/Viruses/enterobacteria_phage_phix174_sensu_lato_uid14015/NC_001422.fna',
+        }
+
+DB_Links = {'UniVec': 'ftp://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec',
+        'EukContam': 'ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/contam_in_euks.fa.gz',
+        'ProkContam': 'ftp://ftp.ncbi.nlm.nih.gov/pub/kitts/contam_in_prok.fa',
+        'Mito': 'ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/mito.nt.gz',
+        }
+
+
