e0f4711433a54327f27457b725e894e5de514f8a
Jason Stajich <jasonstajich.phd@gmail.com>
2018-07-30 23:18:31 -0700

    some basic framework code and configuration

---
+++ AAFTF/AAFTF_main.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+# note structure of code taken from poretools https://github.com/arq5x/poretools/blob/master/poretools/poretools_main.py
+
+import os.path
+import sys
+import argparse
+
+import logging
+logger = logging.getLogger('AAFTF')
+
+# AAFTF imports
+import AAFTF.version
+
+def run_subtool(parser, args):
+    if args.command == 'trim':
+        import trim as submodule
+
+
+class ArgumentParserWithDefaults(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        super(ArgumentParserWithDefaults, self).__init__(*args, **kwargs)
+        self.add_argument("-q", "--quiet", help="Do not output warnings to stderr",
+                            action="store_true",
+                            dest="quiet")
+
+def main():
+    logging.basicConfig()
+
+    #########################################
+    # create the top-level parser
+    #########################################
+    parser = argparse.ArgumentParser(prog='AAFTF', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-v", "--version", help="Installed AAFTF version",
+                        action="version",
+                        version="%(prog)s " + str(AAFTF.version.__version__))
+    subparsers = parser.add_subparsers(title='[sub-commands]', dest='command', parser_class=ArgumentParserWithDefaults)
+
+    #########################################
+    # create the individual tool parsers
+    #########################################
+
+    ##########
+    # combine
+    ##########
+    parser_trim = subparsers.add_parser('trim',
+                                        help='Trim FASTQ input reads and Filter against known or defined contaminant library')
+    # perhaps write this separately for singleton/unpaired read sets
+    parser_trim.add_argument('forward',aliases=['fwd','1'],
+                              dest='forward',
+                              metavar='forward-reads',
+                              required=True,
+                              help='The name of the forward reads of paired-end FASTQ formatted reads.')
+    parser_trim.add_argument('reverse',aliases=['rev','2'],
+                            dest='reverse',
+                            metavar='reverse-reads',
+                            required=True,
+                            help='The name of the reverse reads of paired-end FASTQ formatted reads.')
+
+    parser_trim.set_defaults(func=run_subtool)
+
+    args = parser.parse_args()
+
+    if args.quiet:
+        logger.setLevel(logging.ERROR)
+    try:
+        args.func(parser, args)
+    except IOError as e:
+         if e.errno != 32:  # ignore SIGPIPE
+             raise
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+from setuptools import setup
+
+version_py = os.path.join(os.path.dirname(__file__), 'AAFTF', 'version.py')
+version = open(version_py).read().strip().split('=')[-1].replace('"','').strip()
+print(version)
+
+long_description = """
+``AAFTF`` Automatic Assembly For The Fungi is a toolkit for automated genome assembly, cleanup, and polishing'
+"""
+
+HERE = os.path.dirname(__file__)
+
+with open(os.path.join(HERE, "requirements.txt"), "r") as f:
+    install_requires = [x.strip() for x in f.readlines()]
+
+setup(
+    name="AAFTF",
+    version=version,
+    install_requires=install_requires,
+    requires=['python (>=3.6.0)'],
+    packages=['AAFTF',
+              'AAFTF.scripts'],
+    author="Jason Stajich, Jonathan Palmer",
+    description='Automated genome assembly, cleanup, and polishing',
+    long_description=long_description,
+    url="http://github.com/stajichlab/AAFTF",
+    package_dir={'AAFTF': "AAFTF"},
+    package_data={'AAFTF': []},
+    zip_safe=False,
+    include_package_data=True,
+    #scripts=['AAFTF/scripts/AAFTF'],
+    entry_points={
+        'console_scripts': [
+            'AAFTF=AAFTF.AAFTF_main:main',
+        ],
+    },
+    author_email="jasonstajich.phd@gmail.com",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+        ]
+    )
