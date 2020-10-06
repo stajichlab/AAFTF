edc38d977dc842de645bf670ea45e2b5826d2980
Jason Stajich <jasonstajich.phd@gmail.com>
2020-10-05 21:51:21 -0700

    change the options for extra args

--- AAFTF/AAFTF_main.py
+++ AAFTF/AAFTF_main.py
@@ -262,7 +262,7 @@ def main():
                              help="Print Spades stdout to terminal")
 
     parser_asm.add_argument('--tmpdir',type=str,required=False,help="Assembler temporary dir")
-    parser_asm.add_argument('--assembler_args',type=str,required=False,help="Additional SPAdes/Megahit arguments")
+    parser_asm.add_argument('--assembler_args',action='append',required=False,help="Additional SPAdes/Megahit arguments")
     parser_asm.add_argument('--haplocontigs',dest='haplocontigs',default=False, action='store_true',help="For dipSPAdes take the haplocontigs file")
 
     parser_asm.add_argument('--pipe',action='store_true',
