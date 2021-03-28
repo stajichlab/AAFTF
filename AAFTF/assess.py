bed05a619c9657a87cd32cbfc1ad7023b18a316d
Jason Stajich <jasonstajich.phd@gmail.com>
2021-03-28 14:48:02 -0700

    add GC% to summary stat per #10

--- AAFTF/assess.py
+++ AAFTF/assess.py
@@ -10,12 +10,14 @@ def genome_asm_stats(fasta_file,output_handle):
 
     lengths = []
     # could be smart here and handle compressed files?
+    GC  = 0
     for record in SeqIO.parse(fasta_file, "fasta"):
         lengths.append(len(record))
+        GC += sum(record.seq.count(x) for x in ["G", "C", "g", "c", "S", "s"])
 
     lengths.sort()
     total_len = sum(lengths)
-
+    GC  = 100.0 * (GC / total_len)
     l50 = 0
     n50 = 0
     l90 = 0
@@ -30,7 +32,7 @@ def genome_asm_stats(fasta_file,output_handle):
         if n90 == 0 and cumulsum >= total_len * 0.9:
             n90 = n
             l90 = i
-            
+
         i += 1
     report = "Assembly statistics for: %s\n" % (fasta_file)
     report += "%15s  =  %d\n" % ('CONTIG COUNT',len(lengths))
@@ -43,7 +45,8 @@ def genome_asm_stats(fasta_file,output_handle):
     report += "%15s  =  %d\n" % ('N50',  n50)
     report += "%15s  =  %d\n" % ('L90',  l90)
     report += "%15s  =  %d\n" % ('N90',  n90)
-    
+    report += "%15s  =  %.2f\n" % ('GC%', GC)
+
     print(report)
     if output_handle:
         output_handle.write(report)
@@ -54,9 +57,8 @@ def run(parser,args):
         status("Inputfile %s was not readable, check parameters" % (args.input))
 
     output_handle=None
-    
+
     if args.report:
         output_handle = open(args.report,"w")
 
     genome_asm_stats(args.input, output_handle)
-    
