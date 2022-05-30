2e43557068e09e29379215edea859119bfe16c4f
Jason Stajich <jasonstajich.phd@gmail.com>
2022-05-29 21:34:16 -0700

    updated dipspades testing

--- tests/00_dipspades.sh
+++ tests/00_dipspades.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
+#!/bin/bash -l
 #SBATCH -N 1 -n 24  --mem 96gb --out test_dipspades.%A.log
 
 module load AAFTF
-module switch SPAdes/3.12.0
+module load spades/3.12.0
 which dipspades.py
 which spades.py
 
 MEM=96
-OUTDIR=test
-PREFIX=Rant_dipspades
+OUTDIR=test_dipspades
+PREFIX=Rant
 PHYLUM=Ascomycota
 
 mkdir -p $OUTDIR
-pushd $OUTDIR
-if [ ! -f SRR5223785_1.fastq.gz ]; then
-	curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR522/005/SRR5223785/SRR5223785_1.fastq.gz
-fi
-if [ ! -f SRR5223785_2.fastq.gz ]; then
-	curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR522/005/SRR5223785/SRR5223785_2.fastq.gz
-fi
-popd
+
+SRA=SRR5223785
+FOLDER=$(echo -n $SRA | perl -p -e '$_=sprintf("%s/%03d",substr($_,0,6),substr($_,3,1))')
+URL=ftp://ftp.sra.ebi.ac.uk/vol1/fastq/${FOLDER}/${SRA}/${SRA}
+for DIRECTION in 1 2
+do
+    if [ ! -f $OUTDIR/${SRA}_${DIRECTION}.fastq.gz ]; then
+		echo "downloading $OUTDIR/${SRA}_${DIRECTION}.fastq.gz  from ${URL}_${DIRECTION}.fastq.gz"
+		curl -o $OUTDIR/${SRA}_${DIRECTION}.fastq.gz ${URL}_${DIRECTION}.fastq.gz
+    fi
+done
+
 
 CPU=24
 if [ $SLURM_CPUS_ON_NODE ]; then
@@ -27,19 +31,21 @@ if [ $SLURM_CPUS_ON_NODE ]; then
 fi
 LEFTTRIM=$OUTDIR/${PREFIX}_1P.fastq.gz
 RIGHTTRIM=$OUTDIR/${PREFIX}_2P.fastq.gz
+MERGEDTRIM=$OUTDIR/${PREFIX}_MG.fastq.gz
 LEFT=$OUTDIR/${PREFIX}_filtered_1.fastq.gz
 RIGHT=$OUTDIR/${PREFIX}_filtered_2.fastq.gz
+MERGED=$OUTDIR/${PREFIX}_filtered_U.fastq.gz
 if [ ! -f $LEFTTRIM ]; then
-	../scripts/AAFTF trim --method bbduk --left $OUTDIR/SRR5223785_1.fastq.gz --right $OUTDIR/SRR5223785_2.fastq.gz -o $OUTDIR/${PREFIX} -c $CPU 
+	../scripts/AAFTF trim --method fastp --left $OUTDIR/SRR5223785_1.fastq.gz --right $OUTDIR/SRR5223785_2.fastq.gz -o $OUTDIR/${PREFIX} -c $CPU --merge --dedup
 fi
 if [ ! -f $LEFT ]; then
- 	../scripts/AAFTF filter --mem $MEM -c $CPU --left $LEFTTRIM --right $RIGHTTRIM --aligner bbduk -o $OUTDIR/${PREFIX} 
+ 	../scripts/AAFTF filter --mem $MEM -c $CPU --left $LEFTTRIM --right $RIGHTTRIM --aligner bbduk -o $OUTDIR/${PREFIX}
+	../scripts/AAFTF filter --mem $MEM -c $CPU --left $MERGED --aligner bbduk -o $OUTDIR/${PREFIX}
 fi
-#if [[ -f $LEFT && -f $RIGHT ]]; then
-	#rm -f $LEFTTRIM
-	#rm -f $RIGHTTRIM
-#fi
-
+if [[ -f $LEFT && -f $RIGHT ]]; then
+	rm -f $LEFTTRIM $RIGHTTRIM $MERGEDTRIM
+fi
+exit
 ASMFILE=$OUTDIR/${PREFIX}.dipspades.fasta
 VECCLEAN=$OUTDIR/${PREFIX}.vecscreen.fasta
 PURGE=$OUTDIR/${PREFIX}.sourpurge.fasta
@@ -48,7 +54,7 @@ PILON=$OUTDIR/${PREFIX}.pilon.fasta
 SORTED=$OUTDIR/${PREFIX}.sorted.fasta
 STATS=$OUTDIR/${PREFIX}.sorted.stats.txt
 if [ ! -f $ASMFILE ]; then
-	../scripts/AAFTF assemble --mem $MEM --left $LEFT --right $RIGHT --prefix $OUTDIR/${PREFIX} -c $CPU --method dipspades --workdir dipspades_run
+	../scripts/AAFTF assemble --mem $MEM --left $LEFT --right $RIGHT --merged $MERGED --prefix $OUTDIR/${PREFIX} -c $CPU --method dipspades --workdir dipspades_run
 fi
 exit
 if [ ! -f $VECCLEAN ]; then
