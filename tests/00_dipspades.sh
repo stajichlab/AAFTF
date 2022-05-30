e094fbebc8ecae15633e61f0f94aff4c1901518a
Jason Stajich <jasonstajich.phd@gmail.com>
2022-05-29 22:04:30 -0700

    fixed support for dipspades

--- tests/00_dipspades.sh
+++ tests/00_dipspades.sh
@@ -32,20 +32,35 @@ fi
 LEFTTRIM=$OUTDIR/${PREFIX}_1P.fastq.gz
 RIGHTTRIM=$OUTDIR/${PREFIX}_2P.fastq.gz
 MERGEDTRIM=$OUTDIR/${PREFIX}_MG.fastq.gz
+LEFTTRIM2=$OUTDIR/${PREFIX}_rnd2_1P.fastq.gz
+RIGHTTRIM2=$OUTDIR/${PREFIX}_rnd2_2P.fastq.gz
+
 LEFT=$OUTDIR/${PREFIX}_filtered_1.fastq.gz
 RIGHT=$OUTDIR/${PREFIX}_filtered_2.fastq.gz
 MERGED=$OUTDIR/${PREFIX}_filtered_U.fastq.gz
 if [ ! -f $LEFTTRIM ]; then
-	../scripts/AAFTF trim --method fastp --left $OUTDIR/SRR5223785_1.fastq.gz --right $OUTDIR/SRR5223785_2.fastq.gz -o $OUTDIR/${PREFIX} -c $CPU --merge --dedup
+    ../scripts/AAFTF trim --method fastp \
+		     --left $OUTDIR/SRR5223785_1.fastq.gz \
+		     --right $OUTDIR/SRR5223785_2.fastq.gz \
+		     -o $OUTDIR/${PREFIX} -c $CPU --merge --dedup
+    ../scripts/AAFTF trim --mem $MEM --method bbduk \
+		     --left $LEFTTRIM  \
+		     --right $RIGHTTRIM  \
+		     -o $OUTDIR/${PREFIX}_rnd2 -c $CPU
+
 fi
 if [ ! -f $LEFT ]; then
- 	../scripts/AAFTF filter --mem $MEM -c $CPU --left $LEFTTRIM --right $RIGHTTRIM --aligner bbduk -o $OUTDIR/${PREFIX}
-	../scripts/AAFTF filter --mem $MEM -c $CPU --left $MERGED --aligner bbduk -o $OUTDIR/${PREFIX}
+    ../scripts/AAFTF filter --mem $MEM -c $CPU \
+		     --left $LEFTTRIM2 --right $RIGHTTRIM2 \
+		     --aligner bbduk -o $OUTDIR/${PREFIX}
+    ../scripts/AAFTF filter --mem $MEM -c $CPU \
+		     --left $MERGEDTRIM \
+		     --aligner bbduk -o $OUTDIR/${PREFIX}
 fi
 if [[ -f $LEFT && -f $RIGHT ]]; then
-	rm -f $LEFTTRIM $RIGHTTRIM $MERGEDTRIM
+    rm -f $LEFTTRIM $RIGHTTRIM $MERGEDTRIM $LEFTTRIM2 $RIGHTTRIM2
 fi
-exit
+
 ASMFILE=$OUTDIR/${PREFIX}.dipspades.fasta
 VECCLEAN=$OUTDIR/${PREFIX}.vecscreen.fasta
 PURGE=$OUTDIR/${PREFIX}.sourpurge.fasta
@@ -54,14 +69,17 @@ PILON=$OUTDIR/${PREFIX}.pilon.fasta
 SORTED=$OUTDIR/${PREFIX}.sorted.fasta
 STATS=$OUTDIR/${PREFIX}.sorted.stats.txt
 if [ ! -f $ASMFILE ]; then
-	../scripts/AAFTF assemble --mem $MEM --left $LEFT --right $RIGHT --merged $MERGED --prefix $OUTDIR/${PREFIX} -c $CPU --method dipspades --workdir dipspades_run
+    ../scripts/AAFTF assemble --mem $MEM --left $LEFT --right $RIGHT \
+		     --merged $MERGED --out $ASMFILE -c $CPU \
+		     --method dipspades --workdir $OUTDIR/dipspades
 fi
 exit
 if [ ! -f $VECCLEAN ]; then
 	../scripts/AAFTF vecscreen -i $ASMFILE -o $VECCLEAN -c $CPU
 fi
 if [ ! -f $PURGE ]; then
-	../scripts/AAFTF sourpurge -i  $VECCLEAN -o $PURGE -c $CPU --phylum $PHYLUM --left $LEFT  --right $RIGHT
+    ../scripts/AAFTF sourpurge -i  $VECCLEAN -o $PURGE -c $CPU --phylum $PHYLUM \
+		     --left $LEFT  --right $RIGHT
 fi
 if [ ! -f $CLEANDUP ]; then
 	../scripts/AAFTF rmdup -i $PURGE -o $CLEANDUP -c $CPU -m 1000
