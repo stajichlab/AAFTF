6dd5cadbd4a1051c5c79d69433134f5a4c118101
Jason Stajich <jasonstajich.phd@gmail.com>
2022-08-23 10:22:43 -0700

    init file in tests folder

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2021-2022, Jason Stajich, Jon Palmer.
+#
+# Distributed under the MIT License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
