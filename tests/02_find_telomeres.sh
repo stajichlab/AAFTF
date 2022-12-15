53587b27e4b755c627cf468197ab0b10b0b8697f
Jason Stajich <jasonstajich.phd@gmail.com>
2022-12-14 21:33:42 -0800

    no need to run telomere finding

--- tests/02_find_telomeres.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash -l
-#SBATCH -p short -N 1 -n 4 --mem 4gb
-PREFIX=Rant
-OUTDIR=telomere_reports
-mkdir -p $OUTDIR
-ls test_spades*/$PREFIX.sorted.fasta | parallel -j 4 python find_telomeres.py {} \> $OUTDIR/{//}.{/.}.telomere_report.txt
